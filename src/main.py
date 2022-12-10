e04efe784b11038912b0aa87174686b038746d0b
しゅーた/syuuta <syuuta159@outlook.com>
2022-12-10 22:53:04 +0900

    Basestationを探査からステータスを更新できるようになった。

--- src/main.py
+++ src/main.py
@@ -1,20 +1,33 @@
 import asyncio
 from bleak import BleakScanner
+from bleak import BleakClient
+from asyncio import *
 
+service = "00001525-1212-efde-1523-785feabcd124"
+class main():
+    async def scan(self, loop):
+        devices = await BleakScanner.discover(timeout=5.0)
+        device_list = []
+        for d in devices:
+            devices_name = d.name
+            print(devices_name)
+            if "LHB" in str(devices_name):
+                device_list.append(d)
 
-service = "00001523-1212-efde-1523-785feabcd124"
+        await self.connect(device_list, loop)
 
-async def run():
-    devices = await BleakScanner.discover()
-    device_list = []
-    for d in devices:
-        devices_name = d.name
-        print(devices_name)
-        if "LHB" in str(devices_name):
-            device_list.append(d)
+    async def connect(self, device_list, loop):
+        print(device_list)
+        for d in device_list:
+            async with BleakClient(d.address, loop=loop) as client:
+                x = await client.is_connected()
+                print("Connected: {0}".format(x))
+                y = await client.read_gatt_char(service)
+                # await client.write_gatt_char(service, b'\x00')
+                print(y)
 
-    print(device_list)
 
 if __name__ == '__main__':
+    main = main()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+    loop.run_until_complete(main.scan(loop))
